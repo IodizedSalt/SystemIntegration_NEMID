71ce4f11dc1a8da44c5acad0100b10a803e51f0d
IodizedSalt <christophermoeller2000@gmail.com>
2020-01-16 13:18:37 +0100

    folder restructure

---
+++ assets/js/functions.js
@@ -0,0 +1,43 @@
+function renderSkat(userInformationJSON){
+    document.getElementById("view-button-skat").hidden = false
+
+    document.getElementById("view-button-skat").addEventListener("click", function() {
+        document.getElementById("user-information-json").hidden = false;
+        document.getElementById("user-name").hidden = false;
+        displaySkat(userInformationJSON);
+    }, false);
+
+}
+
+function renderBank(userInformationXML){
+    document.getElementById("view-button-bank").hidden = false
+
+    document.getElementById("view-button-bank").addEventListener("click", function() {
+        document.getElementById("user-information-xml").hidden = false;
+        document.getElementById("user-name").hidden = false;
+        displayBank(userInformationXML);
+    }, false);
+
+}
+
+function displayBank(userInformationXML){
+
+    let bankField = document.getElementById("user-bank-balance");
+    // let xmlDiv = document.getElementById("user-information-xml")
+
+    bankField.innerText = userInformationXML;
+
+}
+
+function displaySkat(userInformationJSON){
+    console.log(userInformationJSON)
+
+
+    let skatField = document.getElementById("user-skat-balance");
+    // let jsonDiv = document.getElementById("user-information-json");
+
+    skatField.innerText = userInformationJSON;  //Why does this not work
+    // skatField.innerHTML = userInformationJSON;
+
+
+}
--- server1.js
+++ server1.js
@@ -7,7 +7,7 @@ const sqlite3 = require('sqlite3').verbose();
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/assets'));
 
 PORT_NUM = 8080
 var skatDatabase = new sqlite3.Database('./db/skat.db');
